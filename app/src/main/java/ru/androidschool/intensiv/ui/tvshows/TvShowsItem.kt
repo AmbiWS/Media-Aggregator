e8f3eb05d92bcc26ec5d01aff405d61f17b498eb
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-06-02 15:08:35 +0300

    Added TvShowsFragment, TvShowsItem

---
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsItem.kt
@@ -0,0 +1,30 @@
+package ru.androidschool.intensiv.ui.tvshows
+
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
+import kotlinx.android.synthetic.main.item_tv_shows.*
+import kotlinx.android.synthetic.main.item_with_text.*
+import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Movie
+
+class TvShowsItem(
+    private val content: Movie,
+    private val onClick: (movie: Movie) -> Unit
+) : Item() {
+
+    override fun getLayout() = R.layout.item_tv_shows
+
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.title_tv_shows.text = content.title
+        viewHolder.tv_shows_rating.rating = content.rating
+        viewHolder.content.setOnClickListener {
+            onClick.invoke(content)
+        }
+
+        // TODO Получать из модели
+        Picasso.get()
+            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .into(viewHolder.image_preview)
+    }
+}
