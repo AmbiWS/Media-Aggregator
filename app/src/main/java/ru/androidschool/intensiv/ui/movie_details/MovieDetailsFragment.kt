c682931fc3e12a200077e4e6640e97debc5850b8
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-06-04 14:28:01 +0300

    Added movie details header: poster, title, button, rating

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -1,6 +1,21 @@
 package ru.androidschool.intensiv.ui.movie_details
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import ru.androidschool.intensiv.R
 
-class MovieDetailsFragment : Fragment(R.layout.movie_details_fragment)
+class MovieDetailsFragment : Fragment(R.layout.movie_details_fragment) {
+
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
