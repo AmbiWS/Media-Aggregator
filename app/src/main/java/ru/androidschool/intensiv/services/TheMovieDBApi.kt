b5a0b1b2e0d59199526354832026b30f8e27c833
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-06-26 22:33:57 +0300

    Api requests covered with 'Observable' type, instead of 'Call'

--- app/src/main/java/ru/androidschool/intensiv/services/TheMovieDBApi.kt
+++ app/src/main/java/ru/androidschool/intensiv/services/TheMovieDBApi.kt
@@ -1,5 +1,6 @@
 package ru.androidschool.intensiv.services
 
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,20 +10,20 @@ import ru.androidschool.intensiv.data.*
 interface TheMovieDBApi {
 
     @GET("movie/now_playing")
-    fun getNowPlayingMovies(@Query("page") page: Int): Call<MovieDBResponse>
+    fun getNowPlayingMovies(@Query("page") page: Int): Observable<MovieDBResponse>
 
     @GET("movie/top_rated")
-    fun getTopRatedMovies(@Query("page") page: Int): Call<MovieDBResponse>
+    fun getTopRatedMovies(@Query("page") page: Int): Observable<MovieDBResponse>
 
     @GET("movie/popular")
-    fun getPopularMovies(@Query("page") page: Int): Call<MovieDBResponse>
+    fun getPopularMovies(@Query("page") page: Int): Observable<MovieDBResponse>
 
     @GET("tv/popular")
-    fun getPopularTvShows(@Query("page") page: Int): Call<MovieDBResponse>
+    fun getPopularTvShows(@Query("page") page: Int): Observable<MovieDBResponse>
 
     @GET("movie/{movie_id}")
-    fun getMovieDetails(@Path("movie_id") id: Int): Call<MovieDetails>
+    fun getMovieDetails(@Path("movie_id") id: Int): Observable<MovieDetails>
 
     @GET("movie/{movie_id}/credits")
-    fun getMovieCredits(@Path("movie_id") id: Int): Call<MovieCredits>
+    fun getMovieCredits(@Path("movie_id") id: Int): Observable<MovieCredits>
 }
