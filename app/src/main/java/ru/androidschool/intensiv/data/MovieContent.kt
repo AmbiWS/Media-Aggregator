6a873bfec1cf9be7e131f8e531924c6c4018eb1a
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-05 17:38:36 +0300

    Added Entity Annotations for Room

--- app/src/main/java/ru/androidschool/intensiv/data/MovieContent.kt
+++ app/src/main/java/ru/androidschool/intensiv/data/MovieContent.kt
@@ -1,14 +1,23 @@
 package ru.androidschool.intensiv.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class MovieContent(
+    @ColumnInfo(name = "name")
     @SerializedName(value = "title", alternate = ["name"])
     var title: String = "",
+    @Ignore
     @SerializedName("vote_average")
     var voteAverage: Double = 0.0,
+    @ColumnInfo(name = "poster")
     @SerializedName("poster_path")
     var posterPath: String? = "",
+    @PrimaryKey
     @SerializedName("id")
     var id: Int = 0
 ) {
