12f9ca8ce4c6c4ccc80a2d59b9e3cf4f23a66ea7
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-05 21:08:45 +0300

    Added favorite movies to Watchlist

--- app/src/main/java/ru/androidschool/intensiv/dao/MovieDao.kt
+++ app/src/main/java/ru/androidschool/intensiv/dao/MovieDao.kt
@@ -2,13 +2,14 @@ package ru.androidschool.intensiv.dao
 
 import androidx.room.*
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
 import ru.androidschool.intensiv.data.MovieContent
 
 @Dao
 interface MovieDao {
 
     @Query("SELECT * FROM movies")
-    fun getAll(): List<MovieContent>
+    fun getAll(): Flowable<List<MovieContent>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertMovie(movie: MovieContent): Completable
--- app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/watchlist/WatchlistFragment.kt
@@ -6,9 +6,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_watchlist.*
 import ru.androidschool.intensiv.R
-import ru.androidschool.intensiv.data.MockRepository
+import ru.androidschool.intensiv.room.MovieDB
+
 
 class WatchlistFragment : Fragment(R.layout.fragment_watchlist) {
 
@@ -16,20 +20,30 @@ class WatchlistFragment : Fragment(R.layout.fragment_watchlist) {
         GroupAdapter<GroupieViewHolder>()
     }
 
+    private val mDisposable = CompositeDisposable()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val movieDao = MovieDB.getInstance(requireContext())?.movieDao()
 
         movies_recycler_view.layoutManager = GridLayoutManager(context, 4)
         movies_recycler_view.adapter = adapter.apply { addAll(listOf()) }
 
-        val moviesList =
-            MockRepository.getMovies().map {
-                MoviePreviewItem(
-                    it
-                ) { movie -> }
-            }.toList()
+        mDisposable.add(movieDao?.getAll()
+            ?.subscribeOn(Schedulers.io())
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe {
+                val moviesList = it.map {
+                    MoviePreviewItem(
+                        it
+                    ) { movie -> }
+                }.toList()
 
-        movies_recycler_view.adapter = adapter.apply { addAll(moviesList) }
+                movies_recycler_view.adapter = adapter.apply { addAll(moviesList) }
+            })
+
+
     }
 
     companion object {
