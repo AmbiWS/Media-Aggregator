f59822b1bb8f35342763607b1e3ace2d939c143c
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-09 17:58:33 +0300

    Added favorite movies count from Room to tabLayout

--- app/src/main/java/ru/androidschool/intensiv/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/profile/ProfileFragment.kt
@@ -7,15 +7,26 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.fragment_profile.*
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.room.MovieDB
+import ru.androidschool.intensiv.ui.watchlist.MoviePreviewItem
+import timber.log.Timber
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var profileTabLayoutTitles: Array<String>
+    private val FAVORITE_MOVIES_INDEX = 0
+    private val MOVIES_TITLE_START_INDEX = 2
+
+    private val mDisposable = CompositeDisposable()
 
     private var profilePageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
@@ -46,6 +57,8 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
         doppelgangerViewPager.registerOnPageChangeCallback(profilePageChangeCallback)
 
+        val movieDao = MovieDB.getInstance(requireContext())?.movieDao()
+
         TabLayoutMediator(tabLayout, doppelgangerViewPager) { tab, position ->
 
             // Выделение первой части заголовка таба
@@ -53,11 +66,44 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
             val title = profileTabLayoutTitles[position]
             // Раздеряем название на части. Первый элемент будет кол-во
             val parts = profileTabLayoutTitles[position].split(" ")
-            val number = parts[0]
-            val spannableStringTitle = SpannableString(title)
-            spannableStringTitle.setSpan(RelativeSizeSpan(2f), 0, number.count(), 0)
+            val number: String
 
-            tab.text = spannableStringTitle
+            if (position == FAVORITE_MOVIES_INDEX) {
+
+                mDisposable.add(movieDao?.getAll()
+                    ?.subscribeOn(Schedulers.io())
+                    ?.observeOn(AndroidSchedulers.mainThread())
+                    ?.subscribe {
+                        val moviesCount = it.map {
+                            MoviePreviewItem(
+                                it
+                            ) { movie -> }
+                        }.toList().size
+
+                        val tabTitle = tabLayout.getTabAt(FAVORITE_MOVIES_INDEX)?.text?.substring(MOVIES_TITLE_START_INDEX)
+                        val tabText = moviesCount.toString() + "\n" + tabTitle?.substring(1)
+                        Timber.d(tabTitle.toString())
+
+                        tabLayout.getTabAt(FAVORITE_MOVIES_INDEX)?.let { it1 ->
+                            loadData(tabText, moviesCount.toString(),
+                                it1
+                            )
+                        }
+
+                        mDisposable.dispose()
+                    })
+
+            }
+
+            number = parts[0]
+            loadData(title, number, tab)
+
         }.attach()
+    }
+
+    private fun loadData(text: String, number: String, tab: TabLayout.Tab) {
+        val spannableStringTitle = SpannableString(text)
+        spannableStringTitle.setSpan(RelativeSizeSpan(2f), 0, number.count(), 0)
+        tab.text = spannableStringTitle
     }
 }
