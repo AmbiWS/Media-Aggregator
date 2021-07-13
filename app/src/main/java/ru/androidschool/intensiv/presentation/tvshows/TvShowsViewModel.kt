38a3d0165ba071b51438189ebc716257ce832d95
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-13 13:40:59 +0300

    Added loadData fun to TvShowsViewModel.kt

--- app/src/main/java/ru/androidschool/intensiv/presentation/tvshows/TvShowsViewModel.kt
+++ app/src/main/java/ru/androidschool/intensiv/presentation/tvshows/TvShowsViewModel.kt
@@ -3,9 +3,15 @@ package ru.androidschool.intensiv.presentation.tvshows
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.androidschool.intensiv.data.repository.TvShowsRepository
 import ru.androidschool.intensiv.data.vo.Movie
+import ru.androidschool.intensiv.domain.extensions.ObservableExtensions.subscribeIoObserveMT
+import ru.androidschool.intensiv.domain.usecase.MoviesUseCase
+import timber.log.Timber
 
 class TvShowsViewModel : ViewModel() {
+
+    private val tvShowsUseCase: MoviesUseCase = MoviesUseCase(TvShowsRepository())
 
     private val movies: MutableLiveData<List<Movie>> by lazy {
         MutableLiveData<List<Movie>>().also {
@@ -18,6 +24,14 @@ class TvShowsViewModel : ViewModel() {
     }
 
     private fun loadMovies() {
-
+        tvShowsUseCase.getMovies()
+            .subscribeIoObserveMT()
+            .subscribe(
+                { i ->
+                    movies.postValue(i)
+                },
+                { t ->
+                    Timber.e(t, t.toString())
+                })
     }
 }
