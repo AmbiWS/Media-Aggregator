898c9c3baff8c759a894833e5c607a80be93f4ea
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-19 01:03:28 +0300

    Minor refactoring

--- app/src/main/java/ru/androidschool/intensiv/presentation/movie_details/MovieDetailsViewModelFactory.kt
+++ app/src/main/java/ru/androidschool/intensiv/presentation/movie_details/MovieDetailsViewModelFactory.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MovieDetailsViewModelFactory(val id: Int, val application: Application) : ViewModelProvider.Factory {
+class MovieDetailsViewModelFactory(val id: Int, private val application: Application) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(Int::class.java, Application::class.java)
