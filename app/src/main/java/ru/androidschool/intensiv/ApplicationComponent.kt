bad4cdd8424deabb57c4e695b39a018fed712a49
Ambi WS Studio <ambi.ws.studio@gmail.com>
2021-07-19 01:18:13 +0300

    Added ApplicationComponent default inject method for MovieDetails

--- app/src/main/java/ru/androidschool/intensiv/ApplicationComponent.kt
+++ app/src/main/java/ru/androidschool/intensiv/ApplicationComponent.kt
@@ -1,7 +1,11 @@
 package ru.androidschool.intensiv
 
+import androidx.fragment.app.Fragment
 import dagger.Component
+import ru.androidschool.intensiv.presentation.movie_details.MovieDetailsFragment
 
 @Component
 interface ApplicationComponent {
+
+    fun inject(fragment: MovieDetailsFragment)
 }
