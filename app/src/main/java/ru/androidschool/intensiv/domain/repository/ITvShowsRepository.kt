21f65dc1d33212ac7d08ee1f1e0b3fde5a50bc3b
AmbiWS <ambi.ws.studio@gmail.com>
2021-07-25 02:35:28 +0300

    Added separate TvShows mapper and Repository

--- app/src/main/java/ru/androidschool/intensiv/domain/repository/ITvShowsRepository.kt
+++ app/src/main/java/ru/androidschool/intensiv/domain/repository/ITvShowsRepository.kt
@@ -2,6 +2,6 @@ package ru.androidschool.intensiv.domain.repository
 
 import ru.androidschool.intensiv.data.vo.Movie
 
-interface TvShowsRepository {
-    fun getTvShows(): List<Movie>
+interface ITvShowsRepository {
+    suspend fun getTvShows(): List<Movie>
 }
--- app/src/main/java/ru/androidschool/intensiv/domain/usecase/TvShowsUseCase.kt
+++ app/src/main/java/ru/androidschool/intensiv/domain/usecase/TvShowsUseCase.kt
@@ -1,9 +1,9 @@
 package ru.androidschool.intensiv.domain.usecase
 
 import ru.androidschool.intensiv.data.vo.Movie
-import ru.androidschool.intensiv.domain.repository.TvShowsRepository
+import ru.androidschool.intensiv.domain.repository.ITvShowsRepository
 
-class TvShowsUseCase(private val repository: TvShowsRepository) {
+class TvShowsUseCase(private val repository: ITvShowsRepository) {
 
     fun getTvShows(): List<Movie> {
 
