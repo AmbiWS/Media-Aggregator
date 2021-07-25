4e4a0f0087aad498db90eb57c4bcc77188b08a0c
AmbiWS <ambi.ws.studio@gmail.com>
2021-07-25 05:11:18 +0300

    Init raw list of MovieContent

--- app/src/test/java/ru/androidschool/intensiv/MappingTest.kt
+++ app/src/test/java/ru/androidschool/intensiv/MappingTest.kt
@@ -20,26 +20,30 @@ class MappingTest {
 
     private lateinit var dtoMapper: MovieMapper
     private lateinit var dtoList: List<Movie>
+    private lateinit var rawDtoList: List<MovieContent>
 
     @Before
     @Throws(Exception::class)
     fun init() {
+
+        rawDtoList = listOf(
+            MovieContent(
+                "Title",
+                3.5,
+                "uri",
+                500
+            ), MovieContent(
+                "Title 2",
+                5.0,
+                "uri2",
+                700
+            )
+        )
+
         dtoList = dtoMapper.toValueObject(
             MovieResponse(
                 1,
-                listOf(
-                    MovieContent(
-                        "Title",
-                        3.5,
-                        "uri",
-                        500
-                    ), MovieContent(
-                        "Title 2",
-                        5.0,
-                        "uri2",
-                        700
-                    )
-                ),
+                rawDtoList,
                 1,
                 1
             )
@@ -48,7 +52,9 @@ class MappingTest {
 
     @Test
     fun checkMapperDtoToVoIsCorrect() {
-
+        for (i in dtoList.indices) {
+            compareDtoToVo(dtoList[i], )
+        }
     }
 
     private fun compareDtoToVo(movie: Movie, movieDto: MovieContent) {
